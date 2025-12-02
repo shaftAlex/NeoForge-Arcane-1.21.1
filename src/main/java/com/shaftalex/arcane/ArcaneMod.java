135f561138ecaad058a601b15ab0117fa7d3b48c
shaftAlex <alexanderscriminger@gmail.com>
2025-12-02 13:51:35 -0500

    Added custom creative mode tabs for mod blocks and items

--- src/main/java/com/shaftalex/arcane/ArcaneMod.java
+++ src/main/java/com/shaftalex/arcane/ArcaneMod.java
@@ -1,6 +1,7 @@
 package com.shaftalex.arcane;
 
 import com.shaftalex.arcane.block.ModBlocks;
+import com.shaftalex.arcane.item.ModCreativeModeTabs;
 import com.shaftalex.arcane.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -33,6 +34,8 @@ public class ArcaneMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/com/shaftalex/arcane/item/ModCreativeModeTabs.java
@@ -0,0 +1,38 @@
+package com.shaftalex.arcane.item;
+
+import com.shaftalex.arcane.ArcaneMod;
+import com.shaftalex.arcane.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ArcaneMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> BISMUTH_ITEMS_TAB = CREATIVE_MODE_TAB.register("bismuth_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.BISMUTH.get()))
+                    .title(Component.translatable("creativetab.arcane.bismuth_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.BISMUTH);
+                        output.accept(ModItems.RAW_BISMUTH);
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> BISMUTH_BLOCKS_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ArcaneMod.MOD_ID, "bismuth_items_tab"))
+                    .title(Component.translatable("creativetab.arcane.bismuth_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.BISMUTH_BLOCK);
+                        output.accept(ModBlocks.BISMUTH_ORE);
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
